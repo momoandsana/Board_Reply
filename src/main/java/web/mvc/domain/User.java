fc553f7356958cd849ad51d6fb7ec826109bab46
hoseok <swiftie52@naver.com>
2024-11-03 11:08:43 +0900

    feat : 디비에 채우기 위한 데이터 코드 추가

--- src/main/java/web/mvc/domain/User.java
+++ src/main/java/web/mvc/domain/User.java
@@ -4,10 +4,7 @@ package web.mvc.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
@@ -15,6 +12,7 @@ import lombok.Setter;
 @Table(name = "users") //테이블이름 변경
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
 	@Id //pk
    private String userId;
--- src/test/java/web/mvc/BoardReplyApplicationTests.java
+++ src/test/java/web/mvc/BoardReplyApplicationTests.java
@@ -1,13 +1,29 @@
 package web.mvc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import web.mvc.domain.User;
+import web.mvc.repository.UserRepository;
 
 @SpringBootTest
 class BoardReplyApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @Rollback(false)
+    public void userInsert(){
+        // User 엔티티 코드에 @Builder 추가하기
+        userRepository.save(User.builder().userId("chan").pwd("1234").name("이창범").build());
+        userRepository.save(User.builder().userId("jang").pwd("1234").name("장희정").build());
+        userRepository.save(User.builder().userId("lee").pwd("1234").name("이가현").build());
     }
 
 }
