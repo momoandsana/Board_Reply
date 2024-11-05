dfab05ed922e1a371c0d10c88bd61c499b963d28
hoseok <swiftie52@naver.com>
2024-11-05 10:38:56 +0900

    feat : querydsl 기능 추가

--- src/main/java/web/mvc/repository/FreeBoardRepository.java
+++ src/main/java/web/mvc/repository/FreeBoardRepository.java
@@ -1,9 +1,10 @@
 package web.mvc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import web.mvc.domain.FreeBoard;
 
-public interface FreeBoardRepository extends JpaRepository<FreeBoard,Long> {
+public interface FreeBoardRepository extends JpaRepository<FreeBoard,Long>, QuerydslPredicateExecutor<FreeBoard> {
 }
 /*
 FreeBoard 필드에 기반해서 기본 함수들이 만들어진다
--- src/main/java/web/mvc/repository/ReplyRepository.java
+++ src/main/java/web/mvc/repository/ReplyRepository.java
@@ -1,7 +1,8 @@
 package web.mvc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import web.mvc.domain.Reply;
 
-public interface ReplyRepository extends JpaRepository<Reply,Long> {
+public interface ReplyRepository extends JpaRepository<Reply,Long>, QuerydslPredicateExecutor<Reply> {
 }
