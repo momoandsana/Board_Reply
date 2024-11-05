f8742df2ba522465c1a28faf84f753a4bf8976c1
hoseok <swiftie52@naver.com>
2024-11-05 10:46:50 +0900

    feat : AppConfig 수정

--- src/main/java/web/mvc/config/AppConfig.java
+++ src/main/java/web/mvc/config/AppConfig.java
@@ -1,4 +1,26 @@
 package web.mvc.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
 public class AppConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory getJPAQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+    /*
+    엔티티->dto
+    dto->엔티티
+     */
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 }
